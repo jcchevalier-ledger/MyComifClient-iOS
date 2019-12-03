d03ba77132ef72327caf3950e34f854cf983436c
Jean-Christophe <jean.christophe.chevalier.3@gmail.com>
2019-12-03 12:22:45 +0100

    Added generated project
    Signed-off-by: Jean-Christophe <jean.christophe.chevalier.3@gmail.com>

---
+++ MyComif'Client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyComif'Client
+//
+//  Created by Jean-Christophe Chevalier on 03/12/2019.
+//  Copyright © 2019 Jean-Christophe Chevalier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
